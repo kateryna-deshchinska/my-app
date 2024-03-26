3a89e98fe67715b16dec02b5b37f622f8aebf45f
kateryna.deshchynska <kateryna.deshchynska@truthmd.com>
2024-03-26 22:39:02 +0200

    2

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -6,4 +6,10 @@ import {Component} from "@angular/core";
   styleUrls: ['./server.component.scss']
 })
 export class ServerComponent {
+  serverId = 10;
+  serverStatus = 'offline';
+
+  getServerStatus() {
+    return this.serverStatus;
+  }
 }
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-servers',
   templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.scss']
 })
-export class ServersComponent {
+export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created!';
+  serverName = 'TestServer';
+  username = '';
+  constructor() {
+    setTimeout(() => {this.allowNewServer = true;}, 2000);
+  }
 
+  ngOnInit() {
+  }
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created! with name ' + this.serverName;
+  }
+
+  onUpdateServerName($event: Event) {
+    this.serverName = ($event.target as HTMLInputElement).value;
+  }
+
+  clearUsername() {
+    this.username = '';
+  }
 }
